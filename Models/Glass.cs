4968a4d9fe9d398cdea153998f37e6030ebc5a8c
Harut Safaryan <safaryanharut@gmail.com>
2021-07-29 16:02:33 +0400

    temp

--- Models/Glass.cs
+++ Models/Glass.cs
@@ -16,8 +16,13 @@ namespace GlassApplication.Models
         public int OrderId { get; set; }
         public Order Order { get; set; }
 
+        [Range(4, 19, ErrorMessage ="Thikness must be 4mm - 19mm")]
         public byte Thickness { get; set; }
+
+        [Range(100, 3210, ErrorMessage = "Width must be 100mm - 3210mm")]
         public ushort Width { get; set; }
+        
+        [Range(100, 6000, ErrorMessage = "Height must be 100mm - 6000mm")]
         public ushort Height { get; set; }
 
         [MaxLength(30)]
